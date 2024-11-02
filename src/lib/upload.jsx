84e713886e0727795d519ffe5265858e1a742374
sarang <sarang.office01@gmail.com>
2024-11-02 14:15:31 +0530

    completed edit profile comp

--- src/lib/upload.jsx
+++ src/lib/upload.jsx
@@ -5,35 +5,37 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 
-const upload =async()=>{
+const upload =async(file)=>{
 
     const storage = getStorage();
 const storageRef = ref(storage, `images/${Date.now()+file.name}`);
 
 const uploadTask = uploadBytesResumable(storageRef, file);
 
-uploadTask.on(
-  "state_changed",
-  (snapshot) => {
-    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    console.log("Upload is " + progress + "% done");
-    switch (snapshot.state) {
-      case "paused":
-        console.log("Upload is paused");
-        break;
-      case "running":
-        console.log("Upload is running");
-        break;
-    }
-  },
-  (error) => {},
-  () => {
-    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-      resolvePath(downloadURL)
-    });
-  }
-);
-
+return new Promise((resolve,reject)=>{
+    uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL)
+          });
+        }
+      );
+      
+})
 
 }
 
--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,64 +1,119 @@
-import React, { useEffect, useState } from 'react'
-import "./ProfileUpdate.css"
-import assets from '../../assets/assets'
-import { onAuthStateChanged } from 'firebase/auth'
-import { doc, getDoc } from 'firebase/firestore'
-import { useNavigate } from 'react-router-dom'
-import { auth, db } from '../../config/firebase'
+import React, { useContext, useEffect, useState } from "react";
+import "./ProfileUpdate.css";
+import assets from "../../assets/assets";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../../config/firebase";
+import { toast } from "react-toastify";
+import upload from "../../lib/upload";
+import { AppContext } from "../../context/AppContext";
 
 const ProfileUpdate = () => {
+  const [image, setImage] = useState(false);
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+  const {setUserData}=useContext(AppContext)
 
-    const [image,setImage]=useState(false)
-    const [name,setName]=useState("")
-    const [bio,setBio]=useState("")
-    const [uid,setUid]=useState("")
-    const [prevImage,setPrevImage]=useState("")
+  const navigate = useNavigate();
 
-    const navigate=useNavigate()
+  const profileUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      if (!prevImage && !name) {
+        toast.error("Upload profile picture");
+      }
 
-    useEffect(()=>{
-        onAuthStateChanged(auth,async(user)=>{
-            if(user){
-                setUid(user.uid)
-                const docRef=doc(db,"users",user.uid)
-                const docSnap=await getDoc(docRef)
+      const docRef = doc(db, "users", uid);
+      if (image) {
+        const imgUrl = await upload(image);
+        setPrevImage(imgUrl);
+        await updateDoc(docRef, {
+          avatar: imgUrl,
+          bio: bio,
+          name: name,
+        });
+      } else {
+        await updateDoc(docRef, {
+          bio: bio,
+          name: name,
+        });
+      }
+      const snap=await getDoc(docRef)
+      setUserData(snap.data())
+      navigate("/chat")
+    } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+    }
+  };
 
-                if(docSnap.data().name){
-                    setName(docSnap.data().name)
-                }
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid);
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
 
-                if(docSnap.data().bio){
-                    setBio(docSnap.data().bio)
-                }
+        if (docSnap.data().name) {
+          setName(docSnap.data().name);
+        }
 
-                if(docSnap.data().avatar){
-                    setPrevImage(docSnap.data().avatar)
-                }
-            }else{
-                navigate("/")
-            }
-        })
-    },[])
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar);
+        }
+      } else {
+        navigate("/");
+      }
+    });
+  }, []);
   return (
-    <div className='profile'>
-        <div className='container'>
-            <form>
-                <h3>Profile Details</h3>
-                <label htmlFor="profile_data">
-                    <input onChange={(e)=>setImage(e.target.files[0])} type="file" accept='.jpg ,.png, .jpeg' id='profile_data' hidden />
-                    <img src={image?URL.createObjectURL(image): assets.avatar_icon} alt="" />
-                   upload profile image
-                </label>
+    <div className="profile">
+      <div className="container">
+        <form onSubmit={profileUpdate}>
+          <h3>Profile Details</h3>
+          <label htmlFor="profile_data">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              accept=".jpg ,.png, .jpeg"
+              id="profile_data"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            upload profile image
+          </label>
 
-                <input onChange={(e)=>setName(e.target.value)} value={name} type="text" placeholder='Your name' />
-                <textarea onChange={(e)=>setBio(e.target.value)} value={bio}  placeholder='Write profile bio' />
-                <button type='submit'>Save</button>
-            </form>
-            <img className='logo' src={image?URL.createObjectURL(image) : assets.logo_icon} alt="" />
-        </div>
-        
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="Your name"
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            placeholder="Write profile bio"
+          />
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="logo"
+          src={image ? URL.createObjectURL(image) :prevImage?prevImage: assets.logo_icon}
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileUpdate
+export default ProfileUpdate;
