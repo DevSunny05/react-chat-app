bf8ab874b91320ce65051bca51d5bf9df6511400
sarang <sarang.office01@gmail.com>
2024-11-03 19:38:55 +0530

    working on search func

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./LeftSidebar.css";
 import assets from "../../assets/assets";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { AppContext } from "../../context/AppContext";
 
 const LeftSidebar = () => {
     const navigate=useNavigate()
+    const {userData}=useContext(AppContext)
+
+    const inputHandler=async(e)=>{
+        try {
+            const input=e.target.value;
+            
+            const userRef=collection(db,"users");
+            const q=query(userRef,where("username","==",input.toLowerCase()))
+
+            const querySnap=await getDocs(q)
+
+            if(!querySnap.empty && querySnap.docs[0].data().id !== userData.id){
+                console.log(querySnap.docs[0].data())
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }   
+
   return (
     <div className="ls">
       <div className="ls-top">
@@ -23,7 +45,7 @@ const LeftSidebar = () => {
 
         <div className="ls-serach">
           <img src={assets.search_icon} alt="" />
-          <input type="text" placeholder="Enter here" />
+          <input onChange={inputHandler}  type="text" placeholder="Search here" />
         </div>
 
       </div>
